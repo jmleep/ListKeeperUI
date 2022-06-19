5c9db902fc60e906835c553578f0fa1f33d5016c
Jordan Leeper <j.m.leep@gmail.com>
2022-06-19 00:12:47 -0500

    Add project files.

---
+++ Data/CustomJsonSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace ListKeeperUI.Data
+{
+    public class CustomJsonSerializer
+    {
+        /*var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.,
+            WriteIndented = true
+        };*/
+
+    }
+}
---
+++ Data/ListKeeperAPIService.cs
@@ -0,0 +1,36 @@
+﻿using ListKeeperAPI.Models;
+using System.Text.Json;
+
+namespace ListKeeperUI.Data
+{
+    public class ListKeeperAPIService
+    {
+        
+        private readonly HttpClient _client;
+
+        public ListKeeperAPIService(HttpClient httpClient)
+    {
+        _client = httpClient;
+    }
+
+        public async Task<List<LKParentList>?> GetParentLists()
+        {
+            List<LKParentList>? parentLists = null;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7194/api/LKParentLists");
+            var response = await _client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (content != null)
+                {
+                    parentLists = JsonSerializer.Deserialize<List<LKParentList>>(content);
+                }
+            }
+
+            return parentLists;
+        }
+    }
+}
